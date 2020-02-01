73c57912d8078c18c28b3d6ac6f0f1e812237b3e
Caaalabash <1121062986@qq.com>
2020-02-02 01:15:19 +0800

    fix: add recover

--- sdk/alioss/index.go
+++ sdk/alioss/index.go
@@ -9,6 +9,12 @@ import (
 var Bucket *oss.Bucket
 
 func init() {
+	// oss.New() endpoint为空字符串时，将直接panic
+	defer func() {
+		if p := recover(); p != nil {
+			fmt.Println("sdk/alioss 初始化失败，将导致上传接口不可用")
+		}
+	}()
 	client, err := oss.New(config.AliOssConfig.EndPoint, config.AliOssConfig.Ak, config.AliOssConfig.Sk)
 	if err != nil {
 		fmt.Println("sdk/alioss 初始化client失败，将导致上传接口不可用")
